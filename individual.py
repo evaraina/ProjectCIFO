3045bb106f91a4177574dc313f06e5534fbd0e8b
fatmaboumelala <149524834+fatmaboumelala@users.noreply.github.com>
2024-05-27 14:43:31 +0100

    Update individual.py

--- individual.py
+++ individual.py
@@ -74,23 +74,41 @@ class Individual:
 
     def hvs_fitness(self, target_repr):
         """
-        Compute the fitness using human visual system (HVS) and a scale of gray.
+        Calcola la fitness utilizzando un modello del sistema visivo umano (HVS).
+
+        Args:
+        - image1 (numpy.ndarray): Prima immagine (valori RGB).
+        - image2 (numpy.ndarray): Seconda immagine (valori RGB).
+
+        Returns:
+        - fitness (float): Valutazione della similarità tra le immagini basata sul modello HVS.
         """
-        # Conversion on a gray scale
+        # Conversione delle immagini in scala di grigi
         gray1 = np.dot(self.representation[..., :3], [0.2989, 0.5870, 0.1140])
         gray2 = np.dot(target_repr[..., :3], [0.2989, 0.5870, 0.1140])
 
+        # Calcolo della differenza assoluta
         diff = np.abs(gray1 - gray2)
 
         # Applicazione di un filtro di attenuazione del contrasto
         attenuation_filter = np.exp(-diff / 100.0)
 
-        # Calcolo della media pesata per ottenere la fitness
-        self.fitness = np.sum(diff)
+        # Calculate the weighted mean to get the fitness
+        new_fitness = np.mean(attenuation_filter)
+
+        # Check if fitness remains constant
+        if new_fitness == self.fitness:
+            self.constant_fitness_generations += 1
+            if self.constant_fitness_generations > 10:
+                raise Exception("Fitness has remained constant for too many generations. Interrupting...")
+        else:
+            self.constant_fitness_generations = 0
+
+        self.fitness = new_fitness
 
         return self.fitness
 
-    def get_fitness_mean(self, target_repr):
+        def get_fitness_mean(self, target_repr):
         """
          Compute the fitness of the individual compare to the target image using the Mean Square Error (MSE).
                Args:
@@ -98,9 +116,20 @@ class Individual:
                Returns:
                    -fitness (float)
                """
-
         # Compute Mean Square Error (MSE)
-        self.fitness = np.mean((self.representation - target_repr) ** 2)
+        new_fitness = np.mean((self.representation - target_repr) ** 2)
+
+        # Check if fitness remains constant
+        if new_fitness == self.fitness:
+            self.constant_fitness_generations += 1
+            if self.constant_fitness_generations > 10:
+                raise Exception("Fitness has remained constant for too many generations. Interrupting...")
+
+        # If fitness changes, reset the counter
+        else:
+            self.constant_fitness_generations = 0
+
+        self.fitness = new_fitness
         return self.fitness
 
 
